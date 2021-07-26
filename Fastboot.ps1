0138e73b9e38162a8b4aad84d62331f1da4098df
Rüdiger Olschewsky <rolschewsky@brendel-software.de>
2021-07-26 10:18:54 +0200

    vids

---
+++ Fastboot.ps1
@@ -0,0 +1,6 @@
+﻿$adbPath = ".\platform-tools\adb.exe"
+
+Start-Process -NoNewWindow -FilePath $adbPath -ArgumentList "--version"
+
+Write-Host -ForegroundColor Yellow 'beliebige Taste zum Beenden drücken...'
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
